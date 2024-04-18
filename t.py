54b459530ac05891e96f3d088760625f308a5be4
Khazan <Khazan@foxmail.com>
2024-04-18 17:08:01 +0800

    Update t.py

--- t.py
+++ t.py
@@ -1,8 +1,12 @@
 # import the OpenAI Python library for calling the OpenAI API
 from openai import OpenAI
+import time
 import os
 
 client = OpenAI(api_key=os.environ.get("sk-", "sk-"))
+
+start = time.time()
+print(start)
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
@@ -11,5 +15,8 @@ completion = client.chat.completions.create(
     {"role": "user", "content": "我正在练习英语，当前的对话场景是旅游出行，我们的对话都应该是旅游相关的，如果我的提问与旅游无关的请你提醒我。请生成一段小于20字的开始文本"}
   ]
 )
+
+end = time.time()
+print(end)
 
 print(completion.choices[0].message)
