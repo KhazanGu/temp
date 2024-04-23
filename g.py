3e7e5345689bc374354919869bd2e36dbe40dd96
Khazan <Khazan@foxmail.com>
2024-04-23 12:10:47 +0800

    Update g.py

--- g.py
+++ g.py
@@ -10,8 +10,7 @@ print(start)
 
 beg = [
     {"role": "system", "content": "You are an english teacher"},
-    {"role": "user", "content": "Please help me recommend a sentence better than \"I would like have one cup of office.\", and in Chinese explain why ."},
-    {"role": "user", "content": "Please output the result with the JSON formate like: {\"content\": \"user\", \"recommend\": \"server\", \"explain\": \"reasion\"}, the vaule of the content key should be the \"I would like have one cup of office\", the vaule of the recommend key should be the recommend sentence, the vaule of the explain key should be the explain sentence."} 
+    {"role": "user", "content": "Please help me generate 100 sentences that are all incorrect grammar."},
   ]
 
 print(beg)
