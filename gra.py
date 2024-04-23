4b3132c53a85b5d19cda6748140d46dc53c4617a
Khazan <Khazan@foxmail.com>
2024-04-23 15:52:32 +0800

    Update gra.py

--- gra.py
+++ gra.py
@@ -6,11 +6,15 @@ def grammary(client, text):
 
   start = time.time()
   print(start)
+
+  content1 = "Please help me recommend a sentence better than \"" + text + "\", and in Simplified Chinese explain why."
   
+  content2 = "Please output the result with the JSON format like: {\"content\": \"\", \"recommend\": \"\", \"explain\": \"\"}, the value of the content key should be the \"" + text + "\", the value of the recommend key should be the recommend sentence, the value of the explain key should in Simplified Chinese be the explain sentence."
+
   beg = [
       {"role": "system", "content": "You are an english teacher"},
-      {"role": "user", f"content": "Please help me recommend a sentence better than \"{text}\", and in Chinese explain why ."},
-      {"role": "user", f"content": "Please output the result with the JSON format like: {\"content\": \"\", \"recommend\": \"\", \"explain\": \"\"}, the value of the content key should be the \"{text}\", the value of the recommend key should be the recommend sentence, the value of the explain key should be the explain sentence."} 
+      {"role": "user", "content": content1},
+      {"role": "user", "content": content2} 
     ]
   
   print(beg)
@@ -22,5 +26,14 @@ def grammary(client, text):
   
   end = time.time()
   print(end)
-  
-  print(completion.choices[0].message)
+
+  content = completion.choices[0].message.content
+
+
+  # Specify the file path
+  file_path = "output.txt"
+
+# Open the file in write mode
+  with open(file_path, 'a') as file:
+    # Iterate over the strings and write each one to the file
+    file.write(content + '\n')  #
