e9dffcacf9fa0d5504e2292d40dc73cee67ebf7f
Khazan <Khazan@foxmail.com>
2024-04-23 17:57:28 +0800

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -25,13 +25,13 @@ def grammary(client, text):
 
   content1 = "Please help me correct a sentence if it has incorrect grammar \"" + text + "\""
   
-  content2 = "Please output the result with a JSON object format like: {\"content\": \"\", \"recommend\": \"\", \"explain\": \"\"}"
-  
-  content3 = "the value of the content key should be the \"" + text + "\"" 
+  content2 = "Please output the result with a JSON object format like: {\"correct\": \"\", \"error\": \"\", \"error_words\": \"\"}"
 
-  content4 = "the value of the recommend key should be a sentence with the correct grammar"
+  content3 = "If the sentence has incorrect grammar, the value of the correct key should be a sentence with the proper grammar, else the value should be an empty string." 
 
-  content5 = "the value of the explain key should be the incorrect grammar words" 
+  content4 = "If the sentence has incorrect grammar, the value of the error key should be the reason for incorrect grammar, else the value should be an empty string." 
+
+  content5 = "If the sentence has incorrect grammar, the value of the error_words key should be the words for incorrect grammar, else the value should be an empty string." 
 
   beg = [
       {"role": "system", "content": "You are an english teacher"},
@@ -57,13 +57,13 @@ def grammary(client, text):
 
   print(content)
 
-  # Specify the file path
-  file_path = "output.txt"
+#   # Specify the file path
+#   file_path = "output.txt"
 
-# Open the file in write mode
-  with open(file_path, 'a') as file:
-    # Iterate over the strings and write each one to the file
-    file.write(content + '\n')  #
+# # Open the file in write mode
+#   with open(file_path, 'a') as file:
+#     # Iterate over the strings and write each one to the file
+#     file.write(content + '\n')  #
 
 
 if __name__ == '__main__':
