c5077eb83200f9d1f1fd1da196a01c62f96abfa2
Khazan <Khazan@foxmail.com>
2024-04-23 12:08:17 +0800

    Update tt.py

--- tt.py
+++ tt.py
@@ -10,7 +10,7 @@ print(start)
 
 beg = [
     {"role": "system", "content": "You are an english teacher"},
-    {"role": "user", "content": "Please help me recommend a sentence better than \"I would like have one cup of office.\", and explain why in chinese."},
+    {"role": "user", "content": "Please help me recommend a sentence better than \"I would like have one cup of office.\", and in Chinese explain why ."},
     {"role": "user", "content": "Please output the result with the JSON formate like: {\"content\": \"user\", \"recommend\": \"server\", \"explain\": \"reasion\"}, the vaule of the content key should be the \"I would like have one cup of office\", the vaule of the recommend key should be the recommend sentence, the vaule of the explain key should be the explain sentence."} 
   ]
 
