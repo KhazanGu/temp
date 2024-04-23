21bca109051e36bea963a5ce93906b480676f5eb
Khazan <Khazan@foxmail.com>
2024-04-23 14:09:06 +0800

    Update g.py

--- g.py
+++ g.py
@@ -1,7 +1,9 @@
 # import the OpenAI Python library for calling the OpenAI API
+# import the OpenAI Python library for calling the OpenAI API
 from openai import OpenAI
 import time
 import os
+import json
 
 client = OpenAI(api_key=os.environ.get("sk-", "sk-"))
 
@@ -9,13 +11,13 @@ start = time.time()
 print(start)
 
 beg = [
-    {"role": "user", "content": "Please help me generate 100 sentences that are all incorrect grammar."},
+    {"role": "user", "content": "Please help me generate a JSON object that contains 100 sentences that are all incorrect grammar."},
   ]
 
 print(beg)
 
 completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
+  model="gpt-3.5-turbo-1106",
   response_format={ "type": "json_object" },
   messages=beg
 )
@@ -23,4 +25,10 @@ completion = client.chat.completions.create(
 end = time.time()
 print(end)
 
-print(completion.choices[0].message)
+content = completion.choices[0].message.content
+
+obj = json.loads(content)
+
+sentences = obj["sentences"]
+
+print(sentences)
